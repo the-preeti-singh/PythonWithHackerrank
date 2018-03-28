abc2c725a3b3169e00fdf7ccc2027ca4f1bb189f
Preeti Singh <preetisingh91997@gmail.com>
2018-03-28 09:49:15 -0700

    Print the indices of each occurrence of  in group A .If it does not appear, print  -1.

--- DefaultDict_Tutorial.py
+++ DefaultDict_Tutorial.py
@@ -1,1 +1,6 @@
-
+from collections import defaultdict
+n, m = map(int, input().split())
+d = defaultdict(list)
+[d[input()].append(i+1) for i in range(n)]
+for j in [' '.join(map(str, d[input()])) or -1 for i in range(m)]: 
+    print (j)
