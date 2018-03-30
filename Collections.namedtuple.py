1506e7900b1dcfbd87d5bc672a15ea938f9b2471
Preeti Singh <preetisingh91997@gmail.com>
2018-03-30 10:27:40 -0700

    Return average marks of students.

--- Collections.namedtuple.py
+++ Collections.namedtuple.py
@@ -1,1 +1,4 @@
-
+from collections import namedtuple
+n, nt = int(input()), namedtuple('Student', input().split())
+sl = [nt(*input().split()) for i in range(n)]
+print('%.2f' % (sum([int(student.MARKS) for student in sl])/n))
