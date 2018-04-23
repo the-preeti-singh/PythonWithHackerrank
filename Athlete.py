f720ec1a49a058e9d45da56e7187071eeedfaa62
Preeti Singh <preetisingh91997@gmail.com>
2018-04-23 10:40:07 -0700

    Print the N lines of the sorted table

--- Athlete.py
+++ Athlete.py
@@ -1,1 +1,11 @@
+#!/bin/python3
 
+import sys
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    rows = [input() for _ in range(N)]
+    K = int(input())
+
+    for row in sorted(rows, key=lambda row: int(row.split()[K])):
+        print(row)   
