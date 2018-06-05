d30ad8a15ec6525b417a3d405cd4c39ceed690a2
Preeti Singh <preetisingh91997@gmail.com>
2018-06-05 09:36:54 -0700

    Print the decoded matrix script

--- Matrix_Script.py
+++ Matrix_Script.py
@@ -1,1 +1,6 @@
-
+import re
+matrix = []
+N,M = map(int, raw_input().split())
+for i in range(N):
+    matrix.append(list(raw_input()))
+print re.sub(r'(?<=[A-Za-z0-9])([^A-Za-z0-9]+)(?=[A-Za-z0-9])',' ',"".join("".join(decode) for decode in zip(*matrix)))
