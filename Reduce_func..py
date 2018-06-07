ce69d43f46c3964f13408b03b8155f2fc3cc11c0
Preeti Singh <preetisingh91997@gmail.com>
2018-06-07 10:24:45 -0700

    Print only one line containing the numerator and denominator of the product of the numbers in the list in its simplest form

--- Reduce_func..py
+++ Reduce_func..py
@@ -1,1 +1,14 @@
+from fractions import Fraction
+from functools import reduce
 
+n = int(input())
+data = [Fraction("/".join(input().split())) for _ in range(n)]
+t = reduce(lambda x, y: x*y, data)
+print(t.numerator, t.denominator)
+
+if __name__ == '__main__':
+    fracs = []
+    for _ in range(int(input())):
+        fracs.append(Fraction(*map(int, input().split())))
+    result = product(fracs)
+    print(*result)
