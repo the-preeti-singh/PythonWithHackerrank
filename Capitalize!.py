4d78574f4311fff02584b419b941618d9551099b
Preeti Singh <preetisingh91997@gmail.com>
2018-06-19 10:09:49 -0700

    Print the capitalized string, S.

--- Capitalize!.py
+++ Capitalize!.py
@@ -1,1 +1,21 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+import string
+def solve(s):
+    return string.capwords(s,sep=' ')
+  
+if __name__ == '__main__':
+  fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+  s = input()
+
+  result = solve(s)
+  
+  fptr.write(result + '\n')
+
+  fptr.close()
