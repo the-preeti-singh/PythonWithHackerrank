87826771ba3b2de3136608f81c01dc2cf354a397
Preeti Singh <preetisingh91997@gmail.com>
2018-06-21 09:31:59 -0700

    Print one line: the name of the winner and their score separated by a space

--- The_Minion_Game.py
+++ The_Minion_Game.py
@@ -1,1 +1,11 @@
-
+def minion_game(s):
+    V = frozenset("AEIOU")
+    n = len(s)
+    ksc = sum(q for c, q in zip(s, range(n, 0, -1)) if c in V)
+    ssc = n * (n + 1) // 2 - ksc
+    if ksc > ssc:
+        print("Kevin {:d}".format(ksc))
+    elif ssc > ksc:
+        print("Stuart {:d}".format(ssc))
+    else:
+        print("Draw")
