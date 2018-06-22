73cfcba3fe04765c1447a3c667a1821496b94c1b
Preeti Singh <preetisingh91997@gmail.com>
2018-06-22 09:51:43 -0700

    Update The_Captain's_Room.py

--- The_Captain's_Room.py
+++ The_Captain's_Room.py
@@ -1,1 +1,10 @@
-
+k = int(input())
+L = str(input()).split(" ")
+family = L
+total = set(L)
+for r in set(L):
+    try:
+        family.remove(r)
+    except:
+        pass
+print ("".join((total - set(family))))
