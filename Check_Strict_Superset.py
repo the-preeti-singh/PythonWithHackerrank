105af5d843702b38c9a5347f4c5267f52439c263
Preeti Singh <preetisingh91997@gmail.com>
2018-06-24 10:22:42 -0700

    Print True if set A is a strict superset of all other N sets.Else false.

--- Check_Strict_Superset.py
+++ Check_Strict_Superset.py
@@ -1,1 +1,6 @@
-
+A = set(map(int,input().split()))
+for _ in range(int(input())):
+    B = set(map(int,input().split()))
+    if(not(A>B)or(len(A)<=len(B))):
+        print("False"); exit()
+print("True")
