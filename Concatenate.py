d211d6439c1b6a1d6d04225f86f76b723c9d9d16
Preeti Singh <preetisingh91997@gmail.com>
2018-07-14 10:25:38 -0700

    Print the concatenated array of size (N+M) x P.

--- Concatenate.py
+++ Concatenate.py
@@ -1,1 +1,13 @@
+import numpy as np
 
+n, m, p = map(int, input().split())    #assign n,m,p
+
+source = " ".join(input() for _ in range(n))  #matrix1
+matrix1 = np.array(source.split(), int)
+matrix1.shape = (n, p)
+
+source = " ".join(input() for _ in range(m)) #matrix2
+matrix2 = np.array(source.split(), int)    
+matrix2.shape = (m, p)
+
+print(np.concatenate((matrix1, matrix2), axis=0)) #vertical axis=0
