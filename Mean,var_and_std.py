d5e0bafa941376a66b2a7d74544af7e9097dd700
Preeti Singh <preetisingh91997@gmail.com>
2018-07-28 10:39:07 -0700

    Print Mean,var and std in separate lines

--- Mean,var_and_std.py
+++ Mean,var_and_std.py
@@ -1,1 +1,14 @@
+import numpy as np
 
+n, m = map(int, input().split())
+nums = []
+
+for _ in range(n):
+    nums.append(list(map(int, input().split())))
+
+np.set_printoptions(sign=' ')
+nums = np.array(nums)
+
+print(np.mean(nums, axis=1))
+print(np.var(nums, axis=0))
+print(np.round(np.std(nums), 12))
