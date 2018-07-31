5e12bfe2f1742929e00647c5ea908316288dba72
Preeti Singh <preetisingh91997@gmail.com>
2018-07-31 10:12:54 -0700

    Update XML2_Find_the_Maximum_Depth.py

--- XML2_Find_the_Maximum_Depth.py
+++ XML2_Find_the_Maximum_Depth.py
@@ -1,1 +1,24 @@
+import xml.etree.ElementTree as etree
 
+maxdepth = 0
+def depth(elem, level):
+    global maxdepth
+    # your code goes here
+    child_depths = [ depth(child,level+1)   for child in elem ]
+    if not child_depths:
+        return level + 1
+    else:
+        fattest_kid = max(child_depths)
+        if level == -1:
+            maxdepth = fattest_kid
+        else:
+            return fattest_kid
+          
+if __name__ == '__main__':
+    n = int(input())
+    xml = ""
+    for i in range(n):
+        xml =  xml + input() + "\n"
+    tree = etree.ElementTree(etree.fromstring(xml))
+    depth(tree.getroot(), -1)
+    print(maxdepth)
