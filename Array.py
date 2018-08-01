fe324786222e2c2bec210e49169b3bb439c5ebf4
Preeti Singh <preetisingh91997@gmail.com>
2018-08-01 10:17:07 -0700

    Print the reverse NumPy array with type float

--- Array.py
+++ Array.py
@@ -1,1 +1,9 @@
+import numpy
 
+def arrays(arr):
+   #revrser array first, convert to float array with numpy
+   return(numpy.array(arr[::-1], float))
+
+arr = input().strip().split(' ')
+result = arrays(arr)
+print(result)
