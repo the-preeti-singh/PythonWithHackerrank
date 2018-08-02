5493aa8bcbd454138db27023ce7b47114b696d29
Preeti Singh <preetisingh91997@gmail.com>
2018-08-02 10:20:56 -0700

    Print respective octal,hexadecimal,decimal and binary values of i.

--- Strings/string_formating.py
+++ Strings/string_formating.py
@@ -1,1 +1,8 @@
-
+n = int(input())
+w = len("{0:b}".format(n))
+for i in range(1,n+1):
+  print ("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=w))
+  
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)  
